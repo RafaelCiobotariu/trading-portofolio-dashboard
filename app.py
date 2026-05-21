2bab59d647533e1eca289e36917bc5b5673191c4
Rafael Ciobotariu <rafael.ciobotariu@yahoo.com>
2026-05-21 19:23:10 +0300

    added pie chart and base database actions

--- app.py
+++ app.py
@@ -1,34 +1,54 @@
 from flask import Flask, render_template, request
 import yfinance as yf
+from database import add_actiune, init_db, get_actiuni
 
 
+
+init_db()  
 app = Flask(__name__)
 
 def get_price(symbol):
     actiune = yf.Ticker(symbol)
-    pret = actiune.info['currentPrice']
+    pret = actiune.info.get('currentPrice') or actiune.info.get('regularMarketPrice') or 0
     return pret
   
   
   
-actiuni = [
-    {"simbol": "AAPL", "cantitate": 10, "pret": get_price("AAPL")},
-    {"simbol": "TSLA", "cantitate": 5, "pret": get_price("TSLA")},
-    {"simbol": "GOOGL", "cantitate": 3, "pret": get_price("GOOGL")},
-]
+actiuni = get_actiuni()
 
-total = sum(actiune['cantitate'] * actiune['pret'] for actiune in actiuni)
+
+
+# total = sum(actiune['cantitate'] * actiune['pret'] for actiune in actiuni)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_trader():
     if request.method == 'POST':
         simbol = request.form['simbol']
         cantitate = int(request.form['cantitate'])
-        print(simbol, cantitate)
-    return render_template('index.html', name='Trader', actiuni=actiuni, total=round(total,2))
+        add_actiune(simbol, cantitate)
+    
+    actiuni_raw = get_actiuni()
+    actiuni = []
+    total = 0
+    
+    for row in actiuni_raw:
+        simbol = row[1]
+        cantitate = row[2]
+        pret = get_price(simbol)
+        valoare = cantitate * pret
+        total += valoare
+        actiuni.append({
+            "simbol": simbol,
+            "cantitate": cantitate,
+            "pret": round(pret,2),
+            "valoare": valoare
+        })
+        
+    simboluri = [a["simbol"] for a in actiuni]
+    valori = [a["valoare"] for a in actiuni]
+    return render_template('index.html', name='Trader', actiuni=actiuni, total=round(total,2), simboluri=simboluri, valori=valori)
     
 
 
-  
-print(get_price("AAPL"))
+
 app.run(debug=True)
--- database.py
+++ database.py
@@ -3,6 +3,21 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    # Create users table
+    cursor.execute('CREATE TABLE IF NOT EXISTS actiuni (id INTEGER PRIMARY KEY, simbol TEXT, cantitate INTEGER)')
     conn.commit()
     conn.close()
+    
+def add_actiune(simbol, cantitate):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO actiuni(simbol, cantitate) VALUES(?, ?)',(simbol,cantitate))
+    conn.commit()
+    conn.close()
+
+def get_actiuni():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM actiuni')
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
